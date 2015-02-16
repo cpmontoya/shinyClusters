12705ff4024d04a5d317486fecd56627868e52d8
Carlos P Montoya <cpmontoya@verizon.net>
2015-02-16 13:52:44 -0500

    Made data selection interactive

--- server.R
+++ server.R
@@ -1,6 +1,9 @@
 library(shiny)
+palette("default")
+palette(adjustcolor(1:8,alpha.f=0.5))
 
 # Define server logic required to plot various variables against mpg
 shinyServer(function(input, output) {
-     output$newPlot<-renderPlot(plot(iris$Petal.Length,iris$sepal.Width,col=iris$Species))
+     output$newPlot<-renderPlot({plot(iris[,input$selectX],iris[,input$selectY],col=iris$Species,pch=19,main="Plot of Selected Data",xlab=input$selectX,ylab=input$selectY)})
+     output$text1<-renderText({paste("You have selected this",input$selectY,"and",input$selectY)})
 })
--- ui.R
+++ ui.R
@@ -1,14 +1,18 @@
 library(shiny)
 
 # Define UI for miles per gallon application
-shinyUI(pageWithSidebar(
+shinyUI(fluidPage(
 
-  # Application title
-  headerPanel("Let's find some Clusters"),
-
-  sidebarPanel(),
-
-  mainPanel(
-       plotOutput('newPlot')
+     # Application title
+     titlePanel("Let's find some Clusters"),
+     sidebarLayout(
+          sidebarPanel(
+               selectInput("selectY",label="Select the data to plot on the y-axis",choices=colnames(iris),selected="Sepal.Width"),
+               selectInput("selectX",label="Select the data to plot on the x-axis",choices=colnames(iris),selected="Petal.Length")
+          ),
+          mainPanel(
+               plotOutput('newPlot'),
+               textOutput("text1")
+          )
      )
 ))
