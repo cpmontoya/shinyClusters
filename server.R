ddd450fc028c30a89b6895947b2678242a0b257b
Carlos P Montoya <cpmontoya@verizon.net>
2015-02-17 02:24:33 -0500

    Added k-means cluster computations

--- server.R
+++ server.R
@@ -4,6 +4,14 @@ palette(adjustcolor(1:8,alpha.f=0.5))
 
 # Define server logic required to plot various variables against mpg
 shinyServer(function(input, output) {
-     output$newPlot<-renderPlot({plot(iris[,input$selectX],iris[,input$selectY],col=iris$Species,pch=19,main="Plot of Selected Data",xlab=input$selectX,ylab=input$selectY)})
-     output$text1<-renderText({paste("You have selected this",input$selectY,"and",input$selectY)})
+     clusterData <- reactive({if (input$Actual){
+               iris$Species
+          }else {
+               kmeans(dataInput(), centers = input$kguess,nstart = 25)$cluster
+          }})
+     dataInput <- reactive({data.frame(iris[input$selectX],iris[input$selectY])})
+     output$newPlot<-renderPlot({plot(dataInput()[,1],dataInput()[,2],col=clusterData(),pch=19,main="Plot of Selected Iris Data",xlab=colnames(dataInput())[1],ylab=colnames(dataInput())[2])})
+    output$text1<-renderText({paste("You have selected ",input$kguess," clusters")})
+    
+    output$wssPlot<-renderPlot({plot(c(1:10),sapply(c(1:10),function(x) kmeans(dataInput(),center=x,nstart=10)$tot.withinss),type='o',xlab="Number of Clusters",ylab="Total deviations within clusters",main= "Choosing the number of Clusters")})
 })
--- ui.R
+++ ui.R
@@ -7,12 +7,15 @@ shinyUI(fluidPage(
      titlePanel("Let's find some Clusters"),
      sidebarLayout(
           sidebarPanel(
-               selectInput("selectY",label="Select the data to plot on the y-axis",choices=colnames(iris),selected="Sepal.Width"),
-               selectInput("selectX",label="Select the data to plot on the x-axis",choices=colnames(iris),selected="Petal.Length")
+               selectInput("selectY",label=h3("Select the data to plot on the y-axis"),choices=colnames(iris),selected="Sepal.Width"),
+               selectInput("selectX",label=h3("Select the data to plot on the x-axis"),choices=colnames(iris),selected="Petal.Length"),
+               sliderInput("kguess", label = h3("Enter the number of clusters to find"),min = 1,max = 10, value = 1),
+               checkboxInput("Actual", label = h3("Show Actual Clusters"), value = FALSE)
           ),
           mainPanel(
                plotOutput('newPlot'),
-               textOutput("text1")
+               textOutput("text1"),
+               plotOutput('wssPlot')
           )
      )
 ))
